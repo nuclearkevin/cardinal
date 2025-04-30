cbe62f5bf5bf2eceaada0159c463c5b2c307fe8a
magnoxemo <walid.ahammed01@gmail.com>
2025-04-29 19:45:19 -0500

    addressing comments from @gonuke

--- include/userobjects/ErrorFractionHeuristicUserObject.h
+++ include/userobjects/ErrorFractionHeuristicUserObject.h
@@ -2,23 +2,25 @@
 
 #include "ClusteringUserObject.h"
 
-class ErrorFractionHeuristicUserObject:public ClusteringUserObject{
+class ErrorFractionHeuristicUserObject : public ClusteringUserObject
+{
 
 public:
-    static InputParameters validParams();
-    ErrorFractionHeuristicUserObject(const InputParameters& params);
+  static InputParameters validParams();
+  ErrorFractionHeuristicUserObject(const InputParameters & params);
+
 protected:
-    virtual bool belongsToCluster(libMesh::Elem* base_element,libMesh::Elem*) override;
-    virtual void execute() override;
-    void extremesFinder();
+  virtual bool belongsToCluster(libMesh::Elem * base_element,
+                                libMesh::Elem * neighbor_elem) override;
+  virtual void execute() override;
+  void extremesFinder();
 
-    Real _max  =0; //maybe not a good presumption
-    Real _min  =0;
+  Real _max;
+  Real _min;
 
-    Real _upper_fraction;
-    Real _lower_fraction;
+  Real _upper_fraction;
+  Real _lower_fraction;
 
-    Real _upper_cut_off;
-    Real _lower_cut_off;
-
+  Real _upper_cut_off;
+  Real _lower_cut_off;
 };
--- src/userobjects/ErrorFractionHeuristicUserObject.C
+++ src/userobjects/ErrorFractionHeuristicUserObject.C
@@ -1,62 +1,67 @@
 #include "ErrorFractionHeuristicUserObject.h"
 
-registerMooseObject("CardinalApp",ErrorFractionHeuristicUserObject);
+registerMooseObject("CardinalApp", ErrorFractionHeuristicUserObject);
 
 InputParameters
-ErrorFractionHeuristicUserObject::validParams(){
-
-    InputParameters param = ClusteringUserObject::validParams();
-    param.addRequiredParam<Real>("upper_fraction","upper percentage of error for the heuristics");
-    param.addRequiredParam<Real>("lower_fraction","lower percentage of error for the heuristics");
-    param.addClassDescription(" mimics the error fraction marker in moose");
-
-    return param;
-}
-ErrorFractionHeuristicUserObject::ErrorFractionHeuristicUserObject(const InputParameters & params):
-                            ClusteringUserObject(params),
-        _upper_fraction(getParam<Real>("upper_fraction")),
-        _lower_fraction(getParam<Real>("lower_fraction"))
+ErrorFractionHeuristicUserObject::validParams()
 {
 
+  InputParameters param = ClusteringUserObject::validParams();
+  param.addRequiredParam<Real>("upper_fraction", "upper percentage of error for the heuristics");
+  param.addRequiredParam<Real>("lower_fraction", "lower percentage of error for the heuristics");
+  param.addClassDescription(" mimics the error fraction marker in moose");
+
+  return param;
+}
+ErrorFractionHeuristicUserObject::ErrorFractionHeuristicUserObject(const InputParameters & params)
+  : ClusteringUserObject(params),
+    _upper_fraction(getParam<Real>("upper_fraction")),
+    _lower_fraction(getParam<Real>("lower_fraction")),
+    _max (0),
+    _min(_std::numeric_limits<Real>::max())
+{
 }
 
 void
-ErrorFractionHeuristicUserObject::extremesFinder(){
+ErrorFractionHeuristicUserObject::extremesFinder()
+{
 
-    double score;
-    for (auto & elem : _mesh.active_element_ptr_range())
+  double score;
+  for (auto & elem : _mesh.active_element_ptr_range())
+  {
+    // should I add any nullptr check?
+    score = getMetricData(elem);
+    if (_max < score)
     {
-        //should I add any nullptr check?
-        score = getMetricData(elem);
-        if (_max<score){
-            _max = score;
-        }
-        if (_min>score){
-            _min = score;
-        }
+      _max = score;
     }
-    _upper_cut_off = (1 - _upper_fraction) *_max;
-    _lower_cut_off = _lower_fraction *(_max - _min) + _min;
-
-
+    if (_min > score)
+    {
+      _min = score;
+    }
+  }
+  _upper_cut_off = (1 - _upper_fraction) * _max;
+  _lower_cut_off = _lower_fraction * (_max - _min) + _min;
 }
 
 void
-ErrorFractionHeuristicUserObject::execute(){
-    applyNoClusteringInitialCondition();
-    extremesFinder(); //should it be inside applyNoClusteringInitialCondition?
-    // that function iterates through the mesh as well
-    // but they don't serve the same purpose
-    findCluster();
+ErrorFractionHeuristicUserObject::execute()
+{
+  applyNoClusteringInitialCondition();
+  extremesFinder();
+  // should it be inside applyNoClusteringInitialCondition?
+  //  that function iterates through the mesh as well
+  //  but they don't serve the same purpose
+  findCluster();
 }
 
 bool
-ErrorFractionHeuristicUserObject::belongsToCluster(libMesh::Elem* base_element, libMesh::Elem* neighbor_elem){
-    if (getMetricData(base_element)>_upper_cut_off and getMetricData(neighbor_elem)>_upper_cut_off){
-        return true;
-    }
-    else if (getMetricData(base_element)<_lower_cut_off and getMetricData(neighbor_elem)<_lower_cut_off){
-        return true;
-    }
-    return false;
+ErrorFractionHeuristicUserObject::belongsToCluster(libMesh::Elem * base_element,
+                                                   libMesh::Elem * neighbor_elem)
+{
+
+  double base_val = getMetricData(base_element);
+  double neighbor_val = getMetricData(neighbor_element);
+  return ((std::min(base_val, neighbor_val) > _upper_cut_off) ||
+          (std::max(base_val, neighbor_val) < _lower_cut_off));
 }
