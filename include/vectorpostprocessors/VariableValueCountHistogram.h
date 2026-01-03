8bd9fb13434613247fdb8175399a5eb2d519eaa2
nuclearkevin <66632997+nuclearkevin@users.noreply.github.com>
2026-01-03 14:21:19 -0600

    Add a histogram vector PP.

---
+++ include/vectorpostprocessors/VariableValueCountHistogram.h
@@ -0,0 +1,66 @@
+/********************************************************************/
+/*                  SOFTWARE COPYRIGHT NOTIFICATION                 */
+/*                             Cardinal                             */
+/*                                                                  */
+/*                  (c) 2021 UChicago Argonne, LLC                  */
+/*                        ALL RIGHTS RESERVED                       */
+/*                                                                  */
+/*                 Prepared by UChicago Argonne, LLC                */
+/*               Under Contract No. DE-AC02-06CH11357               */
+/*                With the U. S. Department of Energy               */
+/*                                                                  */
+/*             Prepared by Battelle Energy Alliance, LLC            */
+/*               Under Contract No. DE-AC07-05ID14517               */
+/*                With the U. S. Department of Energy               */
+/*                                                                  */
+/*                 See LICENSE for full restrictions                */
+/********************************************************************/
+
+#pragma once
+
+#include "ElementVectorPostprocessor.h"
+
+/**
+ * A vector post-processor which computes a histogram of the number of
+ * elements which contain a variable value within the edges of a bin.
+ * This vector post-processor only works with CONSTANT MONOMIAL field
+ * variables, as variables with higher order basis functions could be
+ * double-counted.
+ */
+class VariableValueCountHistogram : public ElementVectorPostprocessor
+{
+public:
+  static InputParameters validParams();
+
+  VariableValueCountHistogram(const InputParameters & parameters);
+
+  virtual void initialize() override;
+  virtual void execute() override;
+  virtual void finalize() override;
+  virtual void threadJoin(const UserObject & y) override;
+
+protected:
+  /// Number of histogram bins.
+  const unsigned int _nbins;
+
+  /// Minimum variable value.
+  const Real _min_value;
+
+  /// Maximum variable value.
+  const Real _max_value;
+
+  /// Bin width.
+  const Real _delta_var;
+
+  /// Coupled variable that is being binned.
+  const VariableValue & _value;
+
+  /// Current quadrature point.
+  unsigned int _qp;
+
+  /// Value mid point of the bin.
+  VectorPostprocessorValue & _bin_center;
+
+  /// Aggregated counts for the given bin.
+  VectorPostprocessorValue & _counts;
+};
---
+++ src/vectorpostprocessors/VariableValueCountHistogram.C
@@ -0,0 +1,97 @@
+/********************************************************************/
+/*                  SOFTWARE COPYRIGHT NOTIFICATION                 */
+/*                             Cardinal                             */
+/*                                                                  */
+/*                  (c) 2021 UChicago Argonne, LLC                  */
+/*                        ALL RIGHTS RESERVED                       */
+/*                                                                  */
+/*                 Prepared by UChicago Argonne, LLC                */
+/*               Under Contract No. DE-AC02-06CH11357               */
+/*                With the U. S. Department of Energy               */
+/*                                                                  */
+/*             Prepared by Battelle Energy Alliance, LLC            */
+/*               Under Contract No. DE-AC07-05ID14517               */
+/*                With the U. S. Department of Energy               */
+/*                                                                  */
+/*                 See LICENSE for full restrictions                */
+/********************************************************************/
+
+#include "VariableValueCountHistogram.h"
+
+#include "MooseVariableFE.h"
+
+registerMooseObject("CardinalApp", VariableValueCountHistogram);
+
+InputParameters
+VariableValueCountHistogram::validParams()
+{
+  auto params = ElementVectorPostprocessor::validParams();
+  params.addClassDescription(
+      "Compute a histogram of element counts binned according to variable values.");
+  params.addParam<unsigned int>("bin_number", 50, "Number of histogram bins");
+  params.addCoupledVar("variable", "Variable to bin element counts by. Must be of type CONSTANT MONOMIAL.");
+  params.addRequiredParam<Real>("min_value", "Minimum variable value");
+  params.addRequiredParam<Real>("max_value", "Maximum variable value");
+
+  return params;
+}
+
+VariableValueCountHistogram::VariableValueCountHistogram(const InputParameters & parameters)
+  : ElementVectorPostprocessor(parameters),
+    _nbins(getParam<unsigned int>("bin_number")),
+    _min_value(getParam<Real>("min_value")),
+    _max_value(getParam<Real>("max_value")),
+    _delta_var((_max_value - _min_value) / _nbins),
+    _value(coupledValue("variable")),
+    _bin_center(declareVector(coupledName("variable"))),
+    _counts(declareVector("counts"))
+{
+  if (coupledComponents("variable") != 1)
+    paramError("variable", "VariableValueCountHistogram only supports variables with a single component!");
+
+  if (getFieldVar("variable", 0)->feType() != FEType(libMesh::CONSTANT, libMesh::MONOMIAL))
+    paramError("variable",
+      "VariableValueCountHistogram only supports CONSTANT MONOMIAL shape functions. Please "
+      "ensure that 'variable' is of type MONOMIAL and order CONSTANT.");
+
+  // initialize the bin center value vector
+  _bin_center.resize(_nbins);
+  for (unsigned i = 0; i < _nbins; ++i)
+    _bin_center[i] = (i + 0.5) * _delta_var + _min_value;
+}
+
+void
+VariableValueCountHistogram::initialize()
+{
+  // Reset the histogram.
+  _counts.assign(_nbins, 0.0);
+}
+
+void
+VariableValueCountHistogram::execute()
+{
+  // Compute the bin index. There should be a single elemental DoF as the variable is
+  // a CONSTANT MONOMIAL field variable.
+  int bin = (_value[0] - _min_value) / _delta_var;
+
+  // Increment the bin in the histogram iff the index exists.
+  if (bin >= 0 && static_cast<unsigned int>(bin) < _nbins)
+    _counts[bin] += 1.0;
+}
+
+void
+VariableValueCountHistogram::finalize()
+{
+  gatherSum(_counts);
+}
+
+void
+VariableValueCountHistogram::threadJoin(const UserObject & y)
+{
+  const auto & uo = static_cast<const VariableValueCountHistogram &>(y);
+  mooseAssert(uo._counts.size() == _counts.size(),
+              "Inconsistent counts vector lengths across threads.");
+
+  for (unsigned int i = 0; i < _counts.size(); ++i)
+    _counts[i] += uo._counts[i];
+}
