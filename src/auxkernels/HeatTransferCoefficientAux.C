688236b929099c236537a41f2b34bb8c231d3b20
ssgudala-bwxt <ssgudala@bwxt.com>
2026-06-27 15:07:32 -0400

    Update MOOSE API usage, disable XDG tests/tools, and fix XDG build flags for Cardinal (#2)
    * MOOSE changed getuserobject() call. This has been fixed in the main branch of Cardinal already.
    
    * Moved -DXDG_ENABLE_MOAB out of the unconditional ENABLE_XDG=yes block and into a nested ENABLE_DAGMC=yes. $\n$ -DXDG_ENABLE_MOAB is now only added to libmesh_CXXFLAGS when both ENABLE_XDG=yes and ENABLE_DAGMC=yes. Previously it was always added whenever XDG was enabled.
    
    * Turned off XDG build tests and tools. $\n$ the XDG unit test executables don't use embree headers via xdg's public interface correctly (embree is linked PRIVATE to xdg, so its include dirs don't propagate to test targets). I'm thinking that Cardinal doesn't need XDG's own tests.
    
    * Update contrib/xdg submodule to ssgudala-bwxt/xdg main
    
    * Revert "Update contrib/xdg submodule to ssgudala-bwxt/xdg main"
    
    This reverts commit 6d8acaa105cf6d2b71c88a967e32924a668d5803.

--- src/auxkernels/HeatTransferCoefficientAux.C
+++ src/auxkernels/HeatTransferCoefficientAux.C
@@ -37,9 +37,9 @@ HeatTransferCoefficientAux::validParams()
 
 HeatTransferCoefficientAux::HeatTransferCoefficientAux(const InputParameters & parameters)
   : AuxKernel(parameters),
-    _heat_flux(getUserObjectBase("heat_flux")),
-    _wall_T(getUserObjectBase("wall_T")),
-    _bulk_T(getUserObjectBase("bulk_T"))
+    _heat_flux(getUserObject<UserObject>("heat_flux")),
+    _wall_T(getUserObject<UserObject>("wall_T")),
+    _bulk_T(getUserObject<UserObject>("bulk_T"))
 {
 }
 
